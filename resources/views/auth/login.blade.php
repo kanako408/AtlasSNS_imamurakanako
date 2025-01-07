12f6fffe76c7d29c7e3becc463f191393382ace6
new_workspece-admin <kon.3.25@gmail.com>
2025-01-07 22:59:34 +0900

    ログイン画面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,38 @@
 <x-logout-layout>
-
+  <!-- <div class="container">
+    <div class="login-container">
+      <p class="login-title">AtlasSNSへようこそ</p> -->
+  <!-- <h2>AtlasSNSへようこそ</h2> -->
   <!-- 適切なURLを入力してください -->
   <!-- {!! Form::open(['url' => '〇〇']) !!} -->
   <!-- {!! Form::open(['url' => route('login')]) !!} -->
   <!-- URLにフォームがPOSTされる -->
-  <form method="POST" action="{{ route('login') }}">
-    @csrf <!-- CSRFトークンの追加 -->
+  <!-- ログインフォーム -->
 
-    <p>AtlasSNSへようこそ</p>
+  <body>
+    <header>
+      <h1>AtlasSNSへようこそ</h1>
+    </header>
+    <div id="container">
+      <form method="POST" action="{{ route('login') }}">
+        @csrf <!-- CSRFトークンの追加 -->
 
-    <label for="email">メールアドレス</label>
-    <input type="text" name="email" class="input">
+        <!-- <p>AtlasSNSへようこそ</p> -->
 
-    <label for="password">パスワード</label>
-    <input type="password" name="password" class="input">
+        <!-- メールアドレス入力 -->
+        <label for="email">メールアドレス</label>
+        <input type="text" name="email" class="input" placeholder="メールアドレスを入力">
 
-    <button type="submit">ログイン</button>
+        <!-- パスワード入力 -->
+        <label for="password">パスワード</label>
+        <input type="password" name="password" class="input" placeholder="パスワードを入力">
 
-    <p><a href="{{ route('register') }}">新規ユーザーの方はこちら</a></p>
-  </form>
+        <!-- ログインボタン -->
+        <button type="submit">ログイン</button>
+      </form>
+      <!-- 新規登録リンク -->
+      <p><a href="{{ route('register') }}">新規ユーザーの方はこちら</a></p>
+
+    </div>
+  </body>
 </x-logout-layout>
