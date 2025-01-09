a7aeb82e8157fc8334282b8e349efc65d6129dd1
new_workspece-admin <kon.3.25@gmail.com>
2025-01-09 22:29:53 +0900

    ログイン画面⓶

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,31 @@
 <x-logout-layout>
-  <!-- <div class="container">
-    <div class="login-container">
-      <p class="login-title">AtlasSNSへようこそ</p> -->
-  <!-- <h2>AtlasSNSへようこそ</h2> -->
-  <!-- 適切なURLを入力してください -->
-  <!-- {!! Form::open(['url' => '〇〇']) !!} -->
-  <!-- {!! Form::open(['url' => route('login')]) !!} -->
-  <!-- URLにフォームがPOSTされる -->
-  <!-- ログインフォーム -->
 
   <body>
-    <header>
-      <h1>AtlasSNSへようこそ</h1>
-    </header>
-    <div id="container">
-      <form method="POST" action="{{ route('login') }}">
-        @csrf <!-- CSRFトークンの追加 -->
+    <div class="login-container">
+      <!-- タイトル部分
+      <h1>Atlas</h1>
+      <h2>Social Network Service</h2> -->
 
-        <!-- <p>AtlasSNSへようこそ</p> -->
+      <!-- フォーム部分 -->
+      <div class="login-form">
+        <p>AtlasSNSへようこそ</p>
+        <form method="POST" action="{{ route('login') }}">
+          @csrf <!-- CSRFトークンの追加 -->
 
-        <!-- メールアドレス入力 -->
-        <label for="email">メールアドレス</label>
-        <input type="text" name="email" class="input" placeholder="メールアドレスを入力">
+          <!-- メールアドレス入力 -->
+          <label for="email">メールアドレス</label>
+          <input type="text" name="email" id="email" placeholder="メールアドレスを入力" required>
 
-        <!-- パスワード入力 -->
-        <label for="password">パスワード</label>
-        <input type="password" name="password" class="input" placeholder="パスワードを入力">
+          <!-- パスワード入力 -->
+          <label for="password">パスワード</label>
+          <input type="password" name="password" id="password" placeholder="パスワードを入力" required>
 
-        <!-- ログインボタン -->
-        <button type="submit">ログイン</button>
-      </form>
-      <!-- 新規登録リンク -->
-      <p><a href="{{ route('register') }}">新規ユーザーの方はこちら</a></p>
+          <!-- ログインボタン -->
+          <button type="submit">ログイン</button>
+        </form>
+        <!-- 新規登録リンク -->
+        <p><a href="{{ route('register') }}">新規ユーザーの方はこちら</a></p>
+      </div>
 
     </div>
   </body>
