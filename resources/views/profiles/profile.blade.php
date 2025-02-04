955eb4eea7a29426a58d019f490662a8c1ce9bc6
new_workspece-admin <kon.3.25@gmail.com>
2025-02-04 23:52:56 +0900

    【HTML・CSS】プロフィール編集ページ

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -22,58 +22,60 @@
     </div>
     @endif
     <!-- プロフィール編集フォームをカード状のコンテナで囲む -->
-    <div class="profile-card">
-      <form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <!-- ファイルを送信する機能がある場合にはフォームにenctype属性の設置も必要 -->
+    <div class="user-list">
+      @csrf
+      <div class="update-item">
         <!-- ログインユーザーのアイコンを表示 -->
         <img src="{{ Auth::user()->getIconUrlAttribute() }}"
           alt="{{ Auth::user()->username }}"
           class="user-icon">
+        <!-- </div> -->
+        <!-- ファイルを送信する機能がある場合にはフォームにenctype属性の設置も必要 -->
+        <form action="{{ route('profile.update') }}" method="POST" enctype="multipart/form-data">
+          {{-- ユーザー名 --}}
+          <div class="form-group">
+            <label for="username">ユーザー名</label>
+            <input type="text" name="username" id="username" class="form-control" value="{{ old('username', Auth::user()->username) }}" required>
+          </div>
+      </div>
 
 
-        {{-- ユーザー名 --}}
-        <div class="form-group">
-          <label for="username">ユーザー名</label>
-          <input type="text" name="username" id="username" class="form-control" value="{{ old('username', Auth::user()->username) }}" required>
-        </div>
-
-        {{-- メールアドレス --}}
-        <div class="form-group">
-          <label for="email">メールアドレス</label>
-          <input type="email" name="email" id="email" class="form-control" value="{{ old('email', Auth::user()->email) }}" required>
-        </div>
+      {{-- メールアドレス --}}
+      <div class="form-group">
+        <label for="email">メールアドレス</label>
+        <input type="email" name="email" id="email" class="form-control" value="{{ old('email', Auth::user()->email) }}" required>
+      </div>
 
 
-        {{-- パスワード --}}
-        <div class="form-group">
-          <label for="password">パスワード</label>
-          <input type="password" name="password" id="password" class="form-control">
-        </div>
+      {{-- パスワード --}}
+      <div class="form-group">
+        <label for="password">パスワード</label>
+        <input type="password" name="password" id="password" class="form-control">
+      </div>
 
-        {{-- パスワード確認 --}}
-        <div class="form-group">
-          <label for="password_confirmation">パスワード確認</label>
-          <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
-        </div>
+      {{-- パスワード確認 --}}
+      <div class="form-group">
+        <label for="password_confirmation">パスワード確認</label>
+        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+      </div>
 
-        {{-- 自己紹介文 --}}
-        <div class="form-group">
-          <label for="bio">自己紹介文</label>
-          <textarea name="bio" id="bio" class=".bio-textarea" maxlength="150">{{ old('bio', Auth::user()->bio) }}</textarea>
-        </div>
+      {{-- 自己紹介文 --}}
+      <div class="form-group">
+        <label for="bio">自己紹介文</label>
+        <textarea name="bio" id="bio" class=".bio-textarea" maxlength="150">{{ old('bio', Auth::user()->bio) }}</textarea>
+      </div>
 
-        {{-- アイコン画像 --}}
-        <div class="form-group">
-          <label for="icon_image">アイコン画像</label>
-          <input type="file" name="icon_image" id="icon_image" class="form-control">
-          @if (Auth::user()->icon_image)
-          <!-- <img src="{{ asset('storage/' . Auth::user()->icon_image) }}" alt="アイコン画像" class="img-thumbnail mt-2" width="150"> -->
-          @endif
-        </div>
+      {{-- アイコン画像 --}}
+      <div class="form-group">
+        <label for="icon_image">アイコン画像</label>
+        <input type="file" name="icon_image" id="icon_image" class="form-control">
+        @if (Auth::user()->icon_image)
+        <!-- <img src="{{ asset('storage/' . Auth::user()->icon_image) }}" alt="アイコン画像" class="img-thumbnail mt-2" width="150"> -->
+        @endif
+      </div>
 
-        {{-- 更新ボタン --}}
-        <button type="submit" class="btn btn-danger">更新</button>
+      {{-- 更新ボタン --}}
+      <button type="submit" class="btn btn-danger">更新</button>
       </form>
     </div>
   </div>
